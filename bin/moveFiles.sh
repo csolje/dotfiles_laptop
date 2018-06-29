55f7c1beddeb6033a6c7a8f6b557bc7e3a97053e
Christian Solje <csolje@gmail.com>
2018-06-29 19:00:13 +0800

    adding comments for description

--- bin/moveFiles.sh
+++ bin/moveFiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# this file is for creating directory with the current date and move all files
+# from one folder on my NAS box to another when the webUI can't handle it
 mkdir /volume1/private/Other/"$(date +%F)"
 mv -f /volume1/backup/untitled\ folder/* /volume1/private/Other/"$(date +%F)"
